868f479e39edc0fb8cebe6b1406d74ce2b0ca638
Klaudia <klawdia.j@o2.pl>
2015-11-25 01:50:24 +0100

    listview + contextmenu

--- app/src/main/java/com/example/klaudia/configapp/ResourcesActivity.java
+++ app/src/main/java/com/example/klaudia/configapp/ResourcesActivity.java
@@ -1,11 +1,17 @@
 package com.example.klaudia.configapp;
 
+import android.app.Activity;
+import android.app.ExpandableListActivity;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ExpandableListView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -14,58 +20,79 @@ import android.view.View;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.widget.Toast;
 
-import com.unnamed.b.atv.model.TreeNode;
-import com.unnamed.b.atv.view.AndroidTreeView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
-public class ResourcesActivity extends AppCompatActivity {
+public class ResourcesActivity extends Activity {
 
-    TreeNode root;
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+
     List<Category> nCategories;
     List<Category> vCategories;
-    AndroidTreeView tView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resources);
-        root = TreeNode.root();
-        tView = new AndroidTreeView(this, root);
+
+        // get the listview
+        expListView = (ExpandableListView) findViewById(R.id.list1);
+
+
         initDB();
+
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+        registerForContextMenu(expListView);
     }
 
     void initDB() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
 
         DBAdapter db = new DBAdapter(getApplicationContext());
         db.openDB();
         nCategories = db.getCategoriesFromType("rzeczowniki");
         //vCategories = db.getCategoriesFromType("czasowniki");
-        TreeNode nParent = new TreeNode("Rzeczowniki");
-       // nParent.se
+
         for (Category c: nCategories) {
-            CategoryHolder.IconTreeItem catItem = new CategoryHolder.IconTreeItem(c.getName(), 1);
-            TreeNode catNode = new TreeNode(catItem).setViewHolder(new CategoryHolder(this));
+            listDataHeader.add(c.getName());
             c.setNodes(db.getNodesFromCategory(c.getName()));
+            ArrayList<String> listNodes = new ArrayList<String>();
             for (Node n: c.getNodes()) {
-                NodeHolder.IconTreeItem nodeItem = new NodeHolder.IconTreeItem(""+n.getId(), 1);
-                TreeNode child1 = new TreeNode(nodeItem).setViewHolder(new NodeHolder(this));
-                catNode.addChild(child1);
+                listNodes.add(""+n.getId());
             }
-            nParent.addChild(catNode);
+            listDataChild.put(c.getName(), listNodes);
         }
-        root.addChild(nParent);
-        ScrollView nounTab = (ScrollView)findViewById(R.id.scrollView);
-        nounTab.addView(tView.getView());
+
     }
 
      @Override
     public void onCreateContextMenu(ContextMenu menu, View v,ContextMenu.ContextMenuInfo menuInfo) {
-        onCreateContextMenu(menu, v, menuInfo);
-        menu.setHeaderTitle("Context Menu");
-        menu.add(0, v.getId(), 0, "Action 1");
-        menu.add(0, v.getId(), 0, "Action 2");
-    }
+         super.onCreateContextMenu(menu, v, menuInfo);
+         ExpandableListView.ExpandableListContextMenuInfo info =
+                 (ExpandableListView.ExpandableListContextMenuInfo) menuInfo;
+         int type =
+                 ExpandableListView.getPackedPositionType(info.packedPosition);
+         int group =
+                 ExpandableListView.getPackedPositionGroup(info.packedPosition);
+         int child =
+                 ExpandableListView.getPackedPositionChild(info.packedPosition);
+//Only create a context menu for child items
+
+             menu.setHeaderTitle("11");
+             menu.add(0, 1, 0, "Read page");
+             menu.add(0, 1, 0, "Edit page");
+
+
+         }
 
     @Override
     public boolean onContextItemSelected(MenuItem item) {
